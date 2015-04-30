17f4f77188c61373c0fe72f8a40d663ede0fb75e
Joshua Lucier <joshua.lucier@gmail.com>
2015-04-29 20:58:26 -0400

    Server's up

---
+++ start.js
@@ -0,0 +1,16 @@
+var http = require('http');
+
+const PORT=8080;
+
+
+function handleRequest(request, response)
+{
+	response.end('It Works!!! Path Hit: ' + request.url);
+}
+
+var server = http.createServer(handleRequest);
+
+
+server.listen(PORT, function(){
+	console.log("Server listening on %s", PORT);
+});
